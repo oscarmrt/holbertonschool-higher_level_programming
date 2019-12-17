9325597360487368453fd71edce4cf11b9042046
oscarmrt <oscarmrt@hotmail.com>
2019-12-17 23:49:20 +0000

    file 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ''
+    for x in my_string:
+        if x == 'C' or x == 'c':
+            continue
+        new_string += x
+    return new_string
