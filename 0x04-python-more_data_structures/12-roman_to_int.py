3915d13751c7fc339264a20382cfd8f40610a728
oscarmrt <oscarmrt@hotmail.com>
2019-12-20 03:19:04 +0000

    modified file 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    conromin = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    cromint = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     val = 0
     if roman_string is not None or roman_string is str:
         for x in roman_string:
-            if x in conromin.keys():
-                val = val + conromin[x]
+            if x in cromint.keys():
+                val = val + cromint[x]
         return val
     else:
         return 0
