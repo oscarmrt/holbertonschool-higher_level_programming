ff5ae3ce7f76c182c6199949d57f6007c99ce07d
oscarmrt <oscarmrt@hotmail.com>
2020-01-09 23:20:08 +0000

    modified file 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -29,6 +29,8 @@ class Square:
             print('')
         else:
             for x in range(self.__size):
+                if self.__position[1] > 0:
+                    print('')
                 for z in range(self.__position[0]):
                     print(' ', end='')
                 for y in range(self.__size):
