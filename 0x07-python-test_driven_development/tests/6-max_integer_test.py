e2dcedb34bab34d662d0cb0d450810ea12c7460e
oscarmrt <oscarmrt@hotmail.com>
2020-01-21 03:13:45 +0000

    modified test file 6

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -26,6 +26,14 @@ class TestMaxInteger(unittest.TestCase):
         list = [1, 2, 'holberton', 6]
         with self.assertRaises(TypeError):
             max_integer(list)
+    
+    def test_int(self):
+        """Test valid integers"""
+        self.assertEqual(max_integer([1, 2, 5, 6]), 6)
+        self.assertEqual(max_integer([10]), 10)
+        self.assertEqual(max_integer([1, 2, 7, 3]), 7)
+        self.assertEqual(max_integer([0, -5, -10]), 0)
+        self.assertEqual(max_integer([-1, -3, -6, -9]), -1)
 
 if __name__ == "__main__":
     unittest.main()
