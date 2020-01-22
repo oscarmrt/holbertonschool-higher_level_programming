e651908e0b66457fe497e5101f9985dcc13779fd
oscarmrt <oscarmrt@hotmail.com>
2020-01-22 02:14:43 +0000

    file 12

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" student module
+"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        n_dict = {}
+        for x in attrs:
+            try:
+                n_dict[x] = self.__dict__[x]
+            except:
+                pass
+        return n_dict
