6f2ff75f62b19e0e0e654297499d7477a1de0948
oscarmrt <oscarmrt@hotmail.com>
2020-01-27 20:03:45 +0000

    modified file 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -19,7 +19,7 @@ class Square(Rectangle):
     def size(self):
         """ size function
         """
-        return self.width
+        return self.size
 
     @size.setter
     def size(self, value):
