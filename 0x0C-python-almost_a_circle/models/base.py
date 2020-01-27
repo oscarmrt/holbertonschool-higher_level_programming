c87ad3b6696e6c5da83f85142c1196e75cd2286c
oscarmrt <oscarmrt@hotmail.com>
2020-01-27 18:25:22 +0000

    functions comments

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -10,6 +10,8 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """ init function
+        """
         if id is not None:
             self.id = id
         else:
@@ -17,6 +19,8 @@ class Base:
             self.id = Base.__nb_objects
 
     def to_json_string(list_dictionaries):
+        """ to_json_string function
+        """
         if list_dictionaries is None or len(list_dictionaries) == 0:
             list_dictionaries = []
         return json.dumps(list_dictionaries)
