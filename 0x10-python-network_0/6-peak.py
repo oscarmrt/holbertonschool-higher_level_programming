40f7481db59dbaa98adc234b1eba1d58ac80de7a
oscarmrt <oscarmrt@hotmail.com>
2020-04-10 04:41:41 +0000

    modified file 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
 
 
-def find_peak_aux(intList, low, high, n):
-    '''auxiliar function to find index of middle element'''
-    mid = int(low + (high - low) / 2)
-
-    if ((mid == 0 or intList[mid - 1] <= intList[mid]) and
-       (mid == n - 1 or intList[mid + 1] <= intList[mid])):
-        return intList[mid]
-    elif (mid > 0 and intList[mid - 1] > intList[mid]):
-        return find_peak_aux(intList, low, (mid - 1), n)
-    else:
-        return find_peak_aux(intList, (mid + 1), high, n)
-
-
 def find_peak(list_of_integers):
     '''function that finds a peak in a list of unsorted integers'''
     n = len(list_of_integers)
+    high = n - 1
+    low = 0
+    mid = int((low + high) / 2)
+
     if n == 0:
         return None
     elif n == 1:
         return list_of_integers[0]
     else:
-        return find_peak_aux(list_of_integers, 0, n - 1, n)
+        while (low < high):
+            if (list_of_integers[mid] > list_of_integers[mid + 1]):
+                high = mid
+            else:
+                low = mid + 1
+        return list_of_integers[low]
