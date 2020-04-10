cb57fe681d7de9242d4b63133895308c389b68ec
oscarmrt <oscarmrt@hotmail.com>
2020-04-10 04:47:27 +0000

    modified file 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,7 +6,6 @@ def find_peak(list_of_integers):
     n = len(list_of_integers)
     high = n - 1
     low = 0
-    mid = int((low + high) / 2)
 
     if n == 0:
         return None
@@ -14,6 +13,7 @@ def find_peak(list_of_integers):
         return list_of_integers[0]
     else:
         while (low < high):
+            mid = int((low + high) / 2)
             if (list_of_integers[mid] > list_of_integers[mid + 1]):
                 high = mid
             else:
