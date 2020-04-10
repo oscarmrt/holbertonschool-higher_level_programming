fd89f3f6870e55344c5ae7a91b498d8e0d596bbe
oscarmrt <oscarmrt@hotmail.com>
2020-04-10 04:03:40 +0000

    modified file 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
 
 
-def find_peak(list_of_integers):
-    '''function that finds a peak in a list of unsorted integers.'''
-    lessP = []
-    equalP = []
-    greaterP= []
-    if len(list_of_integers) > 1:
-        pvt = list_of_integers[0]
-        for x in list_of_integers:
-            if x < pvt:
-                lessP.append(x)
-            elif x == pvt:
-                equalP.append(x)
-            elif x > pvt:
-                greaterP.append(x)
-        if len(greaterP) > 1:
-            return max(greaterP)
-        else:
-            return pvt
+def find_peak_aux(intList, low, high, n):
+    '''auxiliar function to find index of middle element'''
+    mid = int(low + (high - low) / 2)
+
+    if ((mid == 0 or intList[mid - 1] <= intList[mid]) and
+       (mid == n - 1 or intList[mid + 1] <= intList[mid])):
+        return intList[mid]
+    elif (mid > 0 and intList[mid - 1] > intList[mid]):
+        return find_peak_aux(intList, low, (mid - 1), n)
     else:
+        return find_peak_aux(intList, (mid + 1), high, n)
+
+
+def find_peak(list_of_integers):
+    '''function that finds a peak in a list of unsorted integers'''
+    if len(list_of_integers) == 0:
         return None
+    elif len(list_of_integers) == 1:
+        return list_of_integers[0]
+    else:
+        return find_peak_aux(list_of_integers, 0, len(list_of_integers) - 1,
+                             len(list_of_integers))
