3f65906abc1323ed46f53c92da5fca076c834340
oscarmrt <oscarmrt@hotmail.com>
2020-04-16 23:15:38 +0000

    file 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# Write a Python script that takes in a URL, sends a request
+# to the URL and displays the body of the response.
+# If the HTTP status code is greater than or equal to 400,
+# print: Error code: followed by the value of the HTTP status code.
+# You must use the packages requests and sys
+# You are not allowed to import packages other than requests and sys
+# You don’t need to check arguments passed to the script (number or type)
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    response = requests.get(url)
+    if response.status_code < 400:
+        print(response.text)
+    else:
+        print('Error code:', response.status_code)
