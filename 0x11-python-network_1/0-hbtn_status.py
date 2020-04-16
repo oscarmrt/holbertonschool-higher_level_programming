a93adec5b12138554f19d531788c2bcab9b7e2af
oscarmrt <oscarmrt@hotmail.com>
2020-04-16 20:17:38 +0000

    added comments for files 0 and 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
+# Write a Python script that fetches https://intranet.hbtn.io/status
+# You must use the package urllib
+# You are not allowed to import any packages other than urllib
+# The body of the response must be displayed like the
+# following example (tabulation before -)/
+# You must use a with statement
 
 from urllib import request
 
-with request.urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
-    print('Body response:')
-    print('\t- type: {}'.format(type(html)))
-    print('\t- content: {}'.format(html))
-    print('\t- utf8 content: {}'.format(html.decode('utf-8')))
+if __name__ == "__main__":
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode('utf-8')))
