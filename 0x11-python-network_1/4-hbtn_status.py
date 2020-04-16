272d70f045442e0bcc9b2f73553e10078e96744c
oscarmrt <oscarmrt@hotmail.com>
2020-04-16 22:26:34 +0000

    file 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# Write a Python script that fetches https://intranet.hbtn.io/status
+# You must use the package requests
+# You are not allow to import packages other than requests
+# The body of the response must be display like the following
+# example (tabulation before -)
+
+import requests
+
+
+if __name__ == "__main__":
+        response = requests.get('https://intranet.hbtn.io/status')
+        print('Body response:')
+        print('\t- type: {}'.format(type(response.text)))
+        print('\t- content: {}'.format(response.text))
