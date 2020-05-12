cdce439063946c191afe2aa1d0493d0f845e2b58
oscarmrt <oscarmrt@hotmail.com>
2020-05-12 20:27:36 +0000

    file 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (err, res) => {
+  if (err) {
+    return console.log(err);
+  } else {
+    console.log('code: ' + res.statusCode);
+  }
+});
